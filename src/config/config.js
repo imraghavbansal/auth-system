1756a3561f9028a066d606f426ba1bf4a43b53ac
Raghav Bansal <imraghavbansal19@gmail.com>
2026-09-13 22:36:03 +0530

    feat: add user registration

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,12 @@ if(!process.env.MONGO_URI){
     throw new Error("MONGO_URI is not defined in .env file");
 }
 
+if(!process.env.JWT_SECRET){
+    throw new Error("JWT_SECRET is not defined in .env file");
+}
+
 const config = {
-    MONGO_URI: process.env.MONGO_URI
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET
 }
 export default config;
