470991c4285f158c240c00e42813329f1ac08b61
Raghav Bansal <imraghavbansal19@gmail.com>
2026-09-14 16:27:33 +0530

    feat: add user login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,8 +3,15 @@ import * as authController from "../controllers/auth.controller.js";
 const authRouter = Router();
 
 
-
+/**
+ POST /api/auth/register
+ */
 authRouter.post("/register", authController.register);
+
+/**
+ GET /api/auth/login
+ */
+authRouter.get("/login", authController.login); 
 
 
 export default authRouter;
